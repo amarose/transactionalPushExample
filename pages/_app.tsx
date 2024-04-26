da93e4f49e5231f6517a3236bf12f96a963acab4
amarose <adammaroszek1@gmail.com>
2024-04-26 14:44:47 +0200

    move GA4 logic to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,37 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import CookieConsent from 'react-cookie-consent';
+import Script from 'next/script';
 
 declare global {
   interface Window {
     dataLayer: Array<any>;
+    gtag: (...args: any[]) => void;
   }
 }
 
-export default function App({ Component, pageProps }: AppProps) {
+function initializeGA() {
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function (...args: any[]) {
+    window.dataLayer.push(args);
+  };
+  window.gtag('js', new Date());
+  window.gtag('config', 'G-3FV871C4QN', {
+    // Add further configuration to respect user privacy
+    send_page_view: true, // Set to true or false as required
+    anonymize_ip: true, // Anonymizes the IP address of the user
+  });
+}
 
+export default function App({ Component, pageProps }: AppProps) {
+  
   useEffect(() => {
+    const hasConsented = localStorage.getItem('GA_Consent');
+    if (hasConsented === 'true') {
+      initializeGA();
+    }
+
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
         .register('/service-worker.js')
@@ -43,5 +64,28 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-3FV871C4QN"
+        strategy="afterInteractive"
+        onLoad={initializeGA}
+      />
+      <CookieConsent
+        location="bottom"
+        buttonText="I understand"
+        cookieName="GA_Consent"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+        onAccept={() => {
+          localStorage.setItem('GA_Consent', 'true');
+          initializeGA(); // Initialize Google Analytics on accept
+        }}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-declare global {
-  interface Window {
-    dataLayer: Array<any>;
-    gtag: (...args: any[]) => void;
-  }
-}
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <script charSet="UTF-8" src="https://s-eu-1.pushpushgo.com/js/651ff5c87582a8ac33d89ec6.js" async={true}></script>
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3FV871C4QN"></script>
-        <script
-        dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){ dataLayer.push(arguments); }
-              gtag('js', new Date());
-              gtag('config', 'G-3FV871C4QN');
-            `,
-          }}
-        />
       </Head>
       <body>
         <Main />
